7d692f808a242d75c22ebf9932edc0436cbd7425
priyanka <prachii8826@gmail.com>
2026-08-08 23:55:33 +0530

    added files

--- Selenium/src/test/java/select/SelectByIndex.java
+++ Selenium/src/test/java/select/SelectByIndex.java
@@ -1,5 +1,19 @@
 package select;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class SelectByIndex {
+	
+	public static void main(String[] args) {
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		
+		driver.get("https://www.flipkart.com/");
+		
+		
+	}
 
 }
