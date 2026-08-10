06b319ad209d3ae9c26f2a254a9970c03ca3dfd0
priyanka <prachii8826@gmail.com>
2026-08-10 21:29:01 +0530

    added files

---
+++ Selenium/src/test/java/myTrip/MakeMyTrip.java
@@ -0,0 +1,31 @@
+package myTrip;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class MakeMyTrip {
+	
+	public static void main(String[] args) throws InterruptedException{
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		
+		driver.get("https://www.amazon.in/");
+		
+		Thread.sleep(3000);
+		
+				
+		Actions action = new Actions(driver);
+		
+//		action.moveByOffset(1081, 21).moveByOffset(0, 437).click().perform(); 
+		
+		action.keyDown(Keys.CONTROL).moveToElement(driver.findElement(By.linkText("Sell on Amazon"))).click().perform();
+
+		
+	}
+		
+}
