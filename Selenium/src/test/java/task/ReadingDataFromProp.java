b8cc40a391a2626ae35989af540e9181efe8502d
priyanka <prachii8826@gmail.com>
2026-08-15 23:48:52 +0530

    added files

--- Selenium/src/test/java/task/ReadingDataFromProp.java
+++ Selenium/src/test/java/task/ReadingDataFromProp.java
@@ -1,5 +1,29 @@
 package task;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class ReadingDataFromProp {
+	
+	public static void main(String[] args) throws IOException{
+		
+		FileInputStream file = new FileInputStream("/Selenium/src/test/resources/Facebook.properties");
+		
+		Properties prop = new Properties();
+		
+		prop.load(file);
+		
+		String browser = prop.getProperty("browser");
+		String url = prop.getProperty("Url");
+		String email = prop.getProperty("Email");
+		String password = prop.getProperty("Password");
+		System.out.println(browser);
+		System.out.println(url);
+		System.out.println(email);
+		System.out.println(password);
+		
+		
+	}
 
 }
