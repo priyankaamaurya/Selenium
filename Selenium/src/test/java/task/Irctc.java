44209744e9ca2e92b1c9027ccb5f8c05a111692c
priyanka <prachii8826@gmail.com>
2026-08-18 22:11:09 +0530

    added files

---
+++ Selenium/src/test/java/task/Irctc.java
@@ -0,0 +1,17 @@
+package task;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Irctc {
+	
+	public static void main(String[] args) {
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		
+		driver.get("");		
+	
+	}
+}
