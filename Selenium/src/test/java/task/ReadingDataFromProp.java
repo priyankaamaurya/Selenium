103db2f969e724de74010e9ce96d7dba6f06d0df
priyanka <prachii8826@gmail.com>
2026-08-23 23:17:43 +0530

    added files

--- Selenium/src/test/java/task/ReadingDataFromProp.java
+++ Selenium/src/test/java/task/ReadingDataFromProp.java
@@ -11,6 +11,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class ReadingDataFromProp {
 	
@@ -65,10 +67,14 @@ public class ReadingDataFromProp {
 		
 		driver.findElement(By.xpath("//span[text()='Log in']")).click();
 		
-		Thread.sleep(3000);
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
 		
-		String errorMsg = driver.findElement(By.xpath("//*[name()='svg' and @class='x1lliihq x2lah0s x1k90msu x2h7rmj x1qfuztq x1a1m0xk xlup9mm x1kky2od']/../..")).getText(); 
-		
+		String errorMsg = wait.until(
+			    ExpectedConditions.visibilityOfElementLocated(
+			        By.xpath("//div[@role='alert']")
+			    )
+		).getText();
+						
 		FileOutputStream fileOutput = new FileOutputStream("src/test/resources/Facebook.properties");
 		
 		prop.setProperty("errorMsg", errorMsg);
@@ -79,80 +85,3 @@ public class ReadingDataFromProp {
 	}
 
 }
-
-
-
-//public class Facebook {
-//	 static WebDriver driver;
-//
-//	 	
-//    public static void main(String[] args) throws IOException, InterruptedException {
-// 
-//       
-//        FileInputStream file = new FileInputStream(
-//        		"src/test/resources/FacebookData.properties");
-//       
-//        Properties prop = new Properties();
-//
-//        prop.load(file);
-// /Selenium/src/test/resources/FacebookData.properties
-//       
-//        String browser = prop.getProperty("browser");
-//        String url = prop.getProperty("url");
-//        String userName = prop.getProperty("username");
-//        String password = prop.getProperty("password");
-//       
-//        switch (browser) {
-//
-//        case "Chrome":
-//            driver = new ChromeDriver();
-//            break;
-//
-//        case "Firefox":
-//            driver = new FirefoxDriver();
-//            break;
-//
-//        case "Edge":
-//            driver = new EdgeDriver();
-//            break;
-//
-//        default:
-//            System.out.println("Enter a valid browser name");
-//            break;
-//        }
-//
-//        driver.manage().window().maximize();
-//
-//        Thread.sleep(3000);
-//        driver.get(url);
-//
-//       
-//        driver.findElement(By.id("_R_1h6kqsqppb6amH1_")).sendKeys(userName);
-//
-//       
-//        driver.findElement(By.id("_R_1hmkqsqppb6amH1_")).sendKeys(password);
-//
-//        
-//        driver.findElement(By.xpath("//span[text()=\"Log in\"]")).click();
-//
-//        //Thread.sleep(3000);
-//driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
-//        
-//        String errorMessage = driver.findElement(
-//                By.xpath("//*[name()='svg' and @class='x1lliihq x2lah0s x1k90msu x2h7rmj x1qfuztq x1a1m0xk xlup9mm x1kky2od']/../.."))
-//                .getText();
-//////a[@href=\"https://facebook.com/login/identify/\"]"
-//        
-//                FileOutputStream fileOutput = new FileOutputStream(
-//                "src/test/resources/FacebookData.properties");
-//
-//               prop.setProperty("errorMessage", errorMessage);
-//
-//              prop.store(fileOutput, "Facebook login");
-//
-//        fileOutput.close();
-//       
-//
-//        
-//    }
-//}
