57a7c327a8bb98be7baa6b7646ca3f995f33a629
priyanka <prachii8826@gmail.com>
2026-09-07 11:58:02 +0530

    added files

---
+++ com.igp/src/test/java/testNG/AccountCreationTest.java
@@ -0,0 +1,25 @@
+package testNG;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class AccountCreationTest {
+	
+	@Test(priority = 1)
+	public void createAccount() {
+		Reporter.log("Created", true);				
+	}
+	
+	@Test(priority = 3)
+	public void deleteAccount() {
+		Reporter.log("Deleted", true);				
+	}
+	
+	@Test(priority = 2)
+	public void updateAccount() {
+		Reporter.log("Updated", true);				
+	}
+	
+
+}
+
