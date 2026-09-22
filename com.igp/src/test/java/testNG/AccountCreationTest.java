19d9fcc61cd196e3a66cc8248b1fe8de74e1d603
priyanka <prachii8826@gmail.com>
2026-09-22 20:49:55 +0530

    added files

--- com.igp/src/test/java/testNG/AccountCreationTest.java
+++ com.igp/src/test/java/testNG/AccountCreationTest.java
@@ -5,17 +5,17 @@ import org.testng.annotations.Test;
 
 public class AccountCreationTest {
 	
-	@Test(priority = 2)
+	@Test(priority = 1)
 	public void createAcc() {
 		Reporter.log("Account Created", true);				
 	}
 	
-	@Test(priority = 1)
+	@Test(priority = 3)
 	public void deleteAcc() {
 		Reporter.log("Account Deleted", true);				
 	}
 	
-	@Test(priority = 3)
+	@Test(priority = 2)
 	public void updateAcc() {
 		Reporter.log("Account Updated", true);				
 	}
