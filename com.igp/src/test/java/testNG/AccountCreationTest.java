5cd296a6fc0544b96616bd2dbfa317df4b3823fb
priyanka <prachii8826@gmail.com>
2026-09-22 20:48:13 +0530

    added files

--- com.igp/src/test/java/testNG/AccountCreationTest.java
+++ com.igp/src/test/java/testNG/AccountCreationTest.java
@@ -7,7 +7,7 @@ public class AccountCreationTest {
 	
 	@Test(priority = 2)
 	public void createAcc() {
-		Reporter.log("Account Created", false);				
+		Reporter.log("Account Created", true);				
 	}
 	
 	@Test(priority = 1)
